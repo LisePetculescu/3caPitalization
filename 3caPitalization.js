4fe25599d5a5b482de630224d3455a248a0a0379
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-16 11:24:13 +0100

    kk

--- 3caPitalization.js
+++ 3caPitalization.js
@@ -2,10 +2,10 @@
 
 const myName = "Lise";
 
-const cap3 = myName.substring(2,3).toUpperCase();
+const cap3 = myName.substring(2, 3).toUpperCase();
 console.log(cap3);
 
-const smallBefore = myName.substring(0,2).toLowerCase();
+const smallBefore = myName.substring(0, 2).toLowerCase();
 console.log(smallBefore);
 
 const smallAfter = myName.substring(3).toLowerCase();
@@ -13,5 +13,5 @@ console.log(smallAfter);
 
 console.log(`${smallBefore}${cap3}${smallAfter}`);
 
-const newName = `${smallBefore}${cap3}${smallAfter}`
+const newName = `${smallBefore}${cap3}${smallAfter}`;
 console.log(newName);
